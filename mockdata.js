cacb288f5fd529cedfa2e14e13b6413617106f49
azs0264 <138828386+azs0264@users.noreply.github.com>
2023-07-07 19:28:08 -0500

    Add files via upload

--- mockdata.js
+++ mockdata.js
@@ -1,14 +1,14 @@
 var mockdata = [
 {"sapShipmentNumber": "900", "plannedshipdate": "2023-06-20", "customerid": "CA1006", "partnumber": "997-3450",
-"carrierid": "XYZ", "weight": "1500", "height": "60", "length": "60", "width": "60", "aesNumber": "NA"},
+"carrierid": "XYZ", "weight": "1500", "height": "60", "length": "60", "width": "60"},
 {"sapShipmentNumber": "901", "plannedshipdate": "2023-06-25", "customerid": "CB1001", "partnumber": "997-6670",
-"carrierid": "ABA", "weight": "500", "height": "48", "length": "36", "width": "72", "aesNumber": "NA"},
+"carrierid": "ABA", "weight": "500", "height": "48", "length": "36", "width": "72"},
 {"sapShipmentNumber": "908", "plannedshipdate": "2023-06-29", "customerid": "CC4001", "partnumber": "997-3450",
-"carrierid": "UPS", "weight": "1500", "height": "60", "length": "60", "width": "60", "aesNumber": "X2023062854321"},
+"carrierid": "UPS", "weight": "1500", "height": "60", "length": "60", "width": "60"},
 {"sapShipmentNumber": "910", "plannedshipdate": "2023-07-02", "customerid": "CC5001", "partnumber": "998-1150",
-"carrierid": "FDX", "weight": "3500", "height": "121", "length": "101", "width": "105", "aesNumber": "X2023070112345"},
+"carrierid": "FDX", "weight": "3500", "height": "121", "length": "101", "width": "105"},
 {"sapShipmentNumber": "999", "plannedshipdate": "2023-07-14", "customerid": "CC5001", "partnumber": "999-1150",
-"carrierid": "FDX", "weight": "3500", "height": "121", "length": "101", "width": "105", "aesNumber": "X2023070112345"},];   
+"carrierid": "FDX", "weight": "3500", "height": "121", "length": "101", "width": "105"},];   
 document.addEventListener('DOMContentLoaded', function() {
 var tableBody = document.getElementById("shipment-table").getElementsByTagName("tbody")[0];
 var rowsHTML = "";
@@ -24,6 +24,5 @@ rowsHTML += "<tr>" +
 "<td>" + shipment.height + "</td>" +
 "<td>" + shipment.length + "</td>" +
 "<td>" + shipment.width + "</td>" +
-"<td>" + shipment.aesNumber + "</td>" +
 "</tr>";}
 tableBody.innerHTML = rowsHTML;});
