00f6cfe6852de5fc7980ba85a4e9e046fb3ea82d
azs0264 <138828386+azs0264@users.noreply.github.com>
2023-07-07 19:04:49 -0500

    Add files via upload

--- mockdata.js
+++ mockdata.js
@@ -1,13 +1,13 @@
 var mockdata = [
-{"sapshipmentnumber": "900", "plannedshipdate": "2023-06-20", "customerid": "CA1006", "partnumber": "997-3450",
+{"sapShipmentNumber": "900", "plannedshipdate": "2023-06-20", "customerid": "CA1006", "partnumber": "997-3450",
 "carrierid": "XYZ", "weight": "1500", "height": "60", "length": "60", "width": "60", "aesnumber": "NA"},
-{"sapshipmentnumber": "901", "plannedshipdate": "2023-06-25", "customerid": "CB1001", "partnumber": "997-6670",
+{"sapShipmentNumber": "901", "plannedshipdate": "2023-06-25", "customerid": "CB1001", "partnumber": "997-6670",
 "carrierid": "ABA", "weight": "500", "height": "48", "length": "36", "width": "72", "aesNumber": "NA"},
-{"sapshipmentnumber": "908", "plannedshipdate": "2023-06-29", "customerid": "CC4001", "partnumber": "997-3450",
+{"sapShipmentNumber": "908", "plannedshipdate": "2023-06-29", "customerid": "CC4001", "partnumber": "997-3450",
 "carrierid": "UPS", "weight": "1500", "height": "60", "length": "60", "width": "60", "aesNumber": "X2023062854321"},
-{"sapshipmentnumber": "910", "plannedshipdate": "2023-07-02", "customerid": "CC5001", "partnumber": "998-1150",
+{"sapShipmentNumber": "910", "plannedshipdate": "2023-07-02", "customerid": "CC5001", "partnumber": "998-1150",
 "carrierid": "FDX", "weight": "3500", "height": "121", "length": "101", "width": "105", "aesNumber": "X2023070112345"},
-{"sapshipmentnumber": "999", "plannedshipdate": "2023-07-14", "customerid": "CC5001", "partnumber": "999-1150",
+{"sapShipmentNumber": "999", "plannedshipdate": "2023-07-14", "customerid": "CC5001", "partnumber": "999-1150",
 "carrierid": "FDX", "weight": "3500", "height": "121", "length": "101", "width": "105", "aesNumber": "X2023070112345"},];   
 document.addEventListener('DOMContentLoaded', function() {
 var tableBody = document.getElementById("shipment-table").getElementsByTagName("tbody")[0];
@@ -15,7 +15,7 @@ var rowsHTML = "";
 for (var i = 0; i < mockdata.length; i++) {
 var shipment = mockdata[i];
 rowsHTML += "<tr>" +
-"<td>" + shipment.sapshipmentNumber + "</td>" +
+"<td>" + shipment.sapShipmentNumber + "</td>" +
 "<td>" + shipment.plannedshipdate + "</td>" +
 "<td>" + shipment.customerid + "</td>" +
 "<td>" + shipment.partnumber + "</td>" +
