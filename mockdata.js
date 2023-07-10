8c3040cccd6b5deb2dfb66bcd9a5ce0dcdd349e5
azs0264 <138828386+azs0264@users.noreply.github.com>
2023-07-10 08:13:06 -0500

    Add files via upload

--- mockdata.js
+++ mockdata.js
@@ -7,6 +7,16 @@ var mockdata = [
 "carrierid": "UPS", "weight": "1500", "height": "60", "length": "60", "width": "60"},
 {"sapShipmentNumber": "910", "plannedshipdate": "2023-07-02", "customerid": "CC5001", "partnumber": "998-1150",
 "carrierid": "FDX", "weight": "3500", "height": "121", "length": "101", "width": "105"},
+{"sapShipmentNumber": "912", "plannedshipdate": "2023-07-05", "customerid": "CA6001", "partnumber": "997-1150",
+"carrierid": "XYZ", "weight": "2100", "height": "100", "length": "99", "width": "76"},
+{"sapShipmentNumber": "918", "plannedshipdate": "2023-07-07", "customerid": "CC5001", "partnumber": "998-1150",
+"carrierid": "FDX", "weight": "3500", "height": "121", "length": "101", "width": "105"},
+{"sapShipmentNumber": "933", "plannedshipdate": "2023-07-10", "customerid": "CA1006", "partnumber": "997-1670",
+"carrierid": "FDX", "weight": "500", "height": "72", "length": "48", "width": "36"},
+{"sapShipmentNumber": "933", "plannedshipdate": "2023-07-11", "customerid": "CB2001", "partnumber": "998-2676",
+"carrierid": "FDX", "weight": "1000", "height": "84", "length": "48", "width": "48"},
+{"sapShipmentNumber": "934", "plannedshipdate": "2023-07-14", "customerid": "CC3001", "partnumber": "999-1140",
+"carrierid": "FDX", "weight": "1500", "height": "76", "length": "68", "width": "68"},
 {"sapShipmentNumber": "999", "plannedshipdate": "2023-07-14", "customerid": "CC5001", "partnumber": "999-1150",
 "carrierid": "FDX", "weight": "3500", "height": "121", "length": "101", "width": "105"},];   
 document.addEventListener('DOMContentLoaded', function() {
