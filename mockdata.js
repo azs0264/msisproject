200236c6f6f67a00e75a7f4e6c08e69be5e1e1d9
azs0264 <138828386+azs0264@users.noreply.github.com>
2023-07-10 08:27:23 -0500

    Add files via upload

--- mockdata.js
+++ mockdata.js
@@ -4,7 +4,7 @@ var mockdata = [
 {"sapShipmentNumber": "901", "plannedshipdate": "2023-06-25", "customerid": "CB1001", "partnumber": "997-6670",
 "carrierid": "ABA", "weight": "500", "height": "48", "length": "36", "width": "72"},
 {"sapShipmentNumber": "908", "plannedshipdate": "2023-06-29", "customerid": "CC4001", "partnumber": "997-3450",
-"carrierid": "UPS", "weight": "1500", "height": "60", "length": "60", "width": "60"},
+"carrierid": "SPD", "weight": "1500", "height": "60", "length": "60", "width": "60"},
 {"sapShipmentNumber": "910", "plannedshipdate": "2023-07-02", "customerid": "CC5001", "partnumber": "998-1150",
 "carrierid": "FDX", "weight": "3500", "height": "121", "length": "101", "width": "105"},
 {"sapShipmentNumber": "912", "plannedshipdate": "2023-07-05", "customerid": "CA6001", "partnumber": "997-1150",
@@ -12,13 +12,13 @@ var mockdata = [
 {"sapShipmentNumber": "918", "plannedshipdate": "2023-07-07", "customerid": "CC5001", "partnumber": "998-1150",
 "carrierid": "FDX", "weight": "3500", "height": "121", "length": "101", "width": "105"},
 {"sapShipmentNumber": "933", "plannedshipdate": "2023-07-10", "customerid": "CA1006", "partnumber": "997-1670",
-"carrierid": "FDX", "weight": "500", "height": "72", "length": "48", "width": "36"},
+"carrierid": "XYZ", "weight": "500", "height": "72", "length": "48", "width": "36"},
 {"sapShipmentNumber": "933", "plannedshipdate": "2023-07-11", "customerid": "CB2001", "partnumber": "998-2676",
-"carrierid": "FDX", "weight": "1000", "height": "84", "length": "48", "width": "48"},
+"carrierid": "TFR", "weight": "1000", "height": "84", "length": "48", "width": "48"},
 {"sapShipmentNumber": "934", "plannedshipdate": "2023-07-14", "customerid": "CC3001", "partnumber": "999-1140",
 "carrierid": "FDX", "weight": "1500", "height": "76", "length": "68", "width": "68"},
 {"sapShipmentNumber": "999", "plannedshipdate": "2023-07-14", "customerid": "CC5001", "partnumber": "999-1150",
-"carrierid": "FDX", "weight": "3500", "height": "121", "length": "101", "width": "105"},];   
+"carrierid": "TFR", "weight": "3500", "height": "121", "length": "101", "width": "105"},];   
 document.addEventListener('DOMContentLoaded', function() {
 var tableBody = document.getElementById("shipment-table").getElementsByTagName("tbody")[0];
 var rowsHTML = "";
